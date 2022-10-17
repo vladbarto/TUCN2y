0654857fa2e82c6733e89a487d555d6424670b94
vladbarto <utclujvb@gmail.com>
2022-10-17 12:08:18 +0300

    Tutorial Oprisa, versiunea 2 done -> Windows

--- Algoritmi Fundamentali/Tutorial Ciprian Oprisa/OprisaTutorial/main.cpp
+++ Algoritmi Fundamentali/Tutorial Ciprian Oprisa/OprisaTutorial/main.cpp
@@ -9,7 +9,7 @@ Profiler p("secondMin");
 #define STEP_SIZE 100
 #define NR_TESTS 50
 
-void secondMin_v1(int v[], int n, int *min, int *min2)
+void secondMin_v1(const int v[], int n, int *min, int *min2)
 {
     Operation opComp = p.createOperation("v1-comp", n);
     Operation opAtrib = p.createOperation("v1-atrib", n);
@@ -54,6 +54,41 @@ void secondMin_v1(int v[], int n, int *min, int *min2)
     }
 }
 
+void secondMin_v2(const int v[], int n, int *min, int *min2)
+{
+    Operation opComp = p.createOperation("v2-comp", n);
+    Operation opAtrib = p.createOperation("v2-atrib", n);
+    opComp.count();
+    if(v[0] < v[1]){
+        opAtrib.count(2);
+        *min = v[0];
+        *min2 = v[1];
+    }
+    else{
+        opAtrib.count(2);
+        *min = v[1];
+        *min2 =  v[0];
+    }
+
+    for(int i = 2; i < n; i++)
+    {
+        opComp.count();
+        if(v[i] < *min)
+        {
+            opAtrib.count(2);
+            *min2 = *min;
+            *min = v[i];
+        }else{
+            opComp.count();
+            if(v[i] < *min2)
+            {
+                opAtrib.count();
+                *min2 = v[i];
+            }
+        }
+    }
+}
+
 void demo()
 {
     int v[] = {7, 2, 8, 5, 9, 1, 6};
@@ -61,6 +96,8 @@ void demo()
     int min = 0, min2 = 0;
 
     secondMin_v1(v, n, &min, &min2);
+    printf("v1: min = %d, min2 = %d\n", min, min2);
+    secondMin_v2(v, n, &min, &min2);
     printf("v1: min = %d, min2 = %d\n", min, min2);
 }
 
@@ -73,12 +110,21 @@ void perf()
             for(int test = 0; test < NR_TESTS; test++){
                 FillRandomArray(v, n);
                 secondMin_v1(v, n, &min, &min2);
+                secondMin_v2(v, n, &min, &min2);
             }
     }
 
     p.divideValues("v1-atrib", NR_TESTS);
     p.divideValues("v1-comp", NR_TESTS);
     p.addSeries("v1", "v1-atrib", "v1-comp");
+
+    p.divideValues("v2-atrib", NR_TESTS);
+    p.divideValues("v2-comp", NR_TESTS);
+    p.addSeries("v2", "v2-atrib", "v2-comp");
+
+    p.createGroup("atribuiri", "v1-atrib", "v2-atrib");
+    p.createGroup("comparatii", "v1-comp", "v2-comp");
+    p.createGroup("total", "v1", "v2");
 
     p.showReport();
 }
