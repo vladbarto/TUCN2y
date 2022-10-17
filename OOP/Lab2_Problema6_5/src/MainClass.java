9b1c946fdfd042969494df741252e254dca79bb6
vladbarto <utclujvb@gmail.com>
2022-10-17 18:27:37 +0300

    Laborator 3 POO, 17.10.2022, problema 4.3 din indrumatorul lab2; BD Tema 1

--- OOP/Lab2_Problema6_5/src/MainClass.java
+++ OOP/Lab2_Problema6_5/src/MainClass.java
@@ -1,11 +1,11 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class MainClass {
 	public static void main(String[] Args) {
 		
 		//Citim intregi si ii afisam pana cand dam de zero (0).
 		int x, val;
-		Scanner in = new Scanner(System.in);
+		/*Scanner in = new Scanner(System.in);
 		
 		do
 		{
@@ -16,33 +16,35 @@ public class MainClass {
 		
 		//punem un mesaj in care afisam numarul
 		System.out.println("Log: numarul afisat este " + x);
-		}	while (x != 0) ;
+		}	while (x != 0) ;*/
 		
 
 		//-----------------------------------------------------------------------------------------------------
 		//Incercam acum sa citim pana cand apare altceva decat numar intreg
+		int y;
 		
-		do
-		{
+		//do
+		//{
 		//nota: printf-ul din C este EGAL cu urmatorul rahat:
 		System.out.println("Consola asteapta sa introduci un numar int: ");
 		
-		x = in.nextInt();
+		y = Integer.parseInt(nextInt());
 		
 		//aruncam o eroare in cazul in care x-ul citit nu este un int
-		try
+		/*try
 		{
-			x.isDigit();
+			y.isDigit();
 			break;
 		}
 		catch(Exception e)
 		{
-			System.out.println("Ce s-a citit in consola nu e litera! " + e);
-		}
-		
+			System.out.println("Ce s-a citit in consola nu e cifra! " + e);
+		}*/
+		if(y>=0 && y <=9)
 		//punem un mesaj in care afisam numarul
-		System.out.println("Log: numarul afisat este " + x);
-		}	while (x != 0) ;
+			System.out.println("Log: numarul afisat este " + x);
+		//}	while (y.isdigit) ;
+		else System.out.println("no, amu ii bai");
 	}
 
 }
